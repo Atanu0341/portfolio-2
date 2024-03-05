7f94e0c1d94d1e001fd5cac25ecf28abb469d0e8
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-05 21:40:05 +0530

    Add useInView and useEffect hooks to Intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,10 +7,23 @@ import { motion } from "framer-motion"
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "../context/ActiveSectionContextProvider";
+import { useEffect } from "react";
 
 export default function Intro() {
+
+    const { ref, inView } = useInView({ threshold: 0.5 })
+    const { setActiveSection } = useActiveSectionContext()
+
+    useEffect(() => {
+        if (inView) {
+            setActiveSection("Home")
+        }
+    }, [inView, setActiveSection])
+
     return (
-        <section id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+        <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "tween", duration: 0.2, }}>
@@ -30,7 +43,7 @@ export default function Intro() {
             </motion.h1>
 
             <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.1, }}>
-                <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">Contact me here{" "}<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/></Link>
+                <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">Contact me here{" "}<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" /></Link>
                 <a href="/CV.pdf" download className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack">Download CV{" "}<HiDownload className="opacity-60 group-hover:translate-y-1 transition" /></a>
                 <a href="https://www.linkedin.com/in/atanu-majumder-169074220/" target="_blank" className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"><BsLinkedin /></a>
                 <a href="https://github.com/Atanu0341" target="_blank" className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"><FaGithubSquare /></a>
