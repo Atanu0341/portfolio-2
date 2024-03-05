218f56eaa9e15301db423cda03a02764d235820c
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-05 13:41:33 +0530

    Update Intro component with additional text and styling

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion"
 
 export default function Intro() {
     return (
-        <section>
+        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "tween", duration: 0.2, }}>
@@ -16,6 +16,14 @@ export default function Intro() {
                     <motion.span className="absolute bottom-0 right-0 text-4xl" initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring", stiffness: 125, delay: 0.1, duration: 0.7, }}>👋</motion.span>
                 </div>
             </div>
+
+            <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
+                <span className="font-bold">Hello, I'm Atanu.</span> I'm a{" "}
+                <span className="font-bold">full-stack developer</span> with{" "}
+                <span className="font-bold">1 years</span> of experience. I enjoy
+                building <span className="italic">sites & apps</span>. My focus is{" "}
+                <span className="underline">React (Next.js)</span>.
+            </motion.p>
         </section>
     )
 }
