76eabd7a5f1716d63e6b56794bb2b7e524f536af
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-05 21:39:52 +0530

    Add useInView and useActiveSectionContext hooks to Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect } from 'react';
 import SectionHeading from './SectionHeading';
 import { projectsData } from '@/src/lib/data';
 import Project from './Project';
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '../context/ActiveSectionContextProvider';
 
 export default function Projects() {
+
+    const { ref, inView } = useInView({ threshold: 0.5 })
+    const { setActiveSection } = useActiveSectionContext()
+
+    useEffect(() => {
+        if (inView) {
+            setActiveSection("Projects")
+        }
+    }, [inView, setActiveSection])
+
     return (
-        <section id='projects' className='scroll-mt-28'>
+        <section ref={ref} id='projects' className='scroll-mt-28'>
             <SectionHeading>My projects</SectionHeading>
 
             <div>
