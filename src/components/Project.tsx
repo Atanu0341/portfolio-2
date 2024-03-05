1053b4c5332d123fcdc62058e0e6887f0c2270e4
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-05 16:32:39 +0530

    Update framer-motion imports and apply scroll animations

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { projectsData } from "@/src/lib/data";
-import { useScroll } from "framer-motion";
+import { useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import { motion } from "framer-motion"
 
@@ -16,8 +16,11 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
         offset: ["0 1", "1.33 1"]
     })
 
+    const scaleProgress =useTransform(scrollYProgress, [0, 1], [0.8, 1])
+    const opacityProgress =useTransform(scrollYProgress, [0, 1], [0.6, 1])
+
     return (
-        <motion.div ref={ref} style={{ scale: scrollYProgress, opacity: scrollYProgress }} className="group mb-3 sm:mb-8 last:mb-0">
+        <motion.div ref={ref} style={{ scale: scaleProgress, opacity: opacityProgress }} className="group mb-3 sm:mb-8 last:mb-0">
             <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8">
                 <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
                     <h3 className="text-2xl font-semibold">{title}</h3>
