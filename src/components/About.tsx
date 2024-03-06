e95c23170316d10be1dace0ffec7681193b228cd
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-06 10:19:31 +0530

    Refactor About component to use custom hook

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,12 @@
 'use client'
 
 import SectionHeading from "./SectionHeading";
-import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion"
-import { useActiveSectionContext } from "../context/ActiveSectionContextProvider";
-import { useEffect } from "react";
+import { useSectionInView } from "@/src/lib/hooks";
 
 export default function About() {
 
-    const { ref, inView } = useInView({ threshold: 0.75 })
-    const { setActiveSection } = useActiveSectionContext()
-
-    useEffect(() => {
-        if (inView) {
-            setActiveSection("About")
-        }
-    }, [inView, setActiveSection])
+    const { ref } = useSectionInView("About")
 
     return (
         <motion.section ref={ref} id="about" className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.175 }}>
