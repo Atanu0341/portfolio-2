5201d7e9da174b88e5dfb7163f2993468398913d
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-07 14:33:01 +0530

    Update image imports in data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,9 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import BookWorld from "@/public/BookWorld.png";
+import Portfolio from "@/public/Portfolio.png";
 
 export const links = [
   {
@@ -60,28 +59,61 @@ export const experiencesData = [
   },
 ] as const;
 
+export const schoolData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miamimii, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+] as const;
+
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "BookWorld",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      " A seamlessly designed web platform powered by Firebase, ReactJS, and Tailwind CSS, where technology meets literature in style. ",
+    tags: ["React", "Firebase", "Tailwind"],
+    githubUrl: "https://github.com/Atanu0341/Book-Store",
+    liveUrl: "https://book-store-lyart-chi.vercel.app/",
+    imageUrl: BookWorld,
   },
   {
-    title: "rmtDev",
+    title: "Portfolio",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "A digital canvas reflecting my journey in web development, blending design finesse and coding prowess. ",
+    tags: ["React", "Next.js", "Tailwind", "Framer"],
+    githubUrl: "https://github.com/Atanu0341/portfolio-2",
+    liveUrl: "https://atanumajumder.vercel.app/",
+    imageUrl: Portfolio,
   },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-  },
+  // {
+  //   title: "Contact Book",
+  //   description:
+  //     "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+  //   tags: ["React", "TypeScript", "Next.js"],
+  //   githubUrl: "https://github.com/Atanu0341/portfolio-2",
+  //   liveUrl: "https://atanumajumder.vercel.app/",
+  //   imageUrl: wordanalyticsImg,
+  // },
 ] as const;
 
 export const skillsData = [
@@ -94,14 +126,7 @@ export const skillsData = [
   "Node.js",
   "Git",
   "Tailwind",
-  "Prisma",
-  "MongoDB",
   "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
   "Python",
-  "Django",
   "Framer Motion",
 ] as const;
